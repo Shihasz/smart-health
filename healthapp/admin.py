4c76696ffa296ac6db59fac2488e4d1dc1e30c25
name <43496101+Shihasz@users.noreply.github.com>
2020-07-09 13:28:17 +0530

    add disease model

--- healthapp/admin.py
+++ healthapp/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from .models import *
 
-# Register your models here.
 admin.site.register(User)
 admin.site.register(Patient)
 admin.site.register(Doctor)
 admin.site.register(Department)
 admin.site.register(Appointment)
+admin.site.register(Disease)
